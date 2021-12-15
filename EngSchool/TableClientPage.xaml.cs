083563b9305052a202221623bce15ad350019568
AitiLar <nparada@bk.ru>
2021-12-15 20:36:41 +0700

    first commit

---
+++ EngSchool/TableClientPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EngSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для TableClientPage.xaml
+    /// </summary>
+    public partial class TableClientPage : Page
+    {
+        public TableClientPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
